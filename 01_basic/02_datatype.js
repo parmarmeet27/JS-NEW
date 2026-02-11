69193a9c04200ace796c777b3f6b51fe7432c10c
parmarmeet27 <128117805+parmarmeet27@users.noreply.github.com>
2026-02-11 02:30:48 +0000

    change succeful

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3+3) we are using nodejs, not browser
+
+let isloggedIn = false;
+let state;
+let name = "hitesh"
+let age = 18;
+
+console.log(3+3)
+console.log("hitesh")
+
+//number
+//bigint
+// boolean => true/false
+//null => standalone value
+//undefined => 
+ //symbol   +> unique value
+
+//object
+
+console.log( typeof null) // return object
+console.log( typeof undefined) // undefined
+
