9eec2e40386523dec0da90678bba0ec604540fa7
parmarmeet27 <128117805+parmarmeet27@users.noreply.github.com>
2026-02-11 03:29:42 +0000

    Change Done

---
+++ 01_basic/3_converstion.js
@@ -0,0 +1,30 @@
+let score = "meet";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let IsLogging = "meet";
+
+let Isbooleanlogging = Boolean(IsLogging);
+
+console.log(Isbooleanlogging);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let scoreNumber = 33;
+
+let ScoreString = String(scoreNumber);
+
+console.log(ScoreString);
+
+console.log(typeof scoreNumber);
